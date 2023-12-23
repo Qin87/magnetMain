94c9df4d54da2f9d98e75cabeb04a6c5fdc42ae6
Qin87 <qinjiang19871018@gmail.com>
2023-12-23 20:44:24 +0000

    sym

--- src/ArgsBen.py
+++ src/ArgsBen.py
@@ -8,14 +8,14 @@ def parse_args():
     # change frequently
     parser.add_argument('--withAug', type=bool, default=False, help='with Aug or not')
     parser.add_argument('--AugDirect', type=int, default=20, help='1 for one direction, 2 for bidirection aug edges, 4 for bidegree and bidirection, 20 for my bidegree(best), 21 for graphSHA bidegree')
-    parser.add_argument('--method_name', type=str, default='GIN', help='method name')   # Tested OK: APPNP
+    parser.add_argument('--method_name', type=str, default='SymDiGCN', help='method name')   # Tested OK: APPNP
     parser.add_argument('--seed', type=int, default=100,
                         help='random seed for training testing split/random graph generation')
     parser.add_argument('--GPUdevice', type=int, default=0, help='gpu 0,1,2 for selene')
 
     # change less frequentl
-    parser.add_argument('--IsDirectedData', type=bool, default=False, help='the dataset is directed graph')
-    parser.add_argument('--dataset', type=str, default='WebKB/texas', help='data set selection')
+    parser.add_argument('--IsDirectedData', type=bool, default=True, help='the dataset is directed graph')
+    parser.add_argument('--dataset', type=str, default='WikipediaNetwork/squirrel', help='data set selection')
     parser.add_argument('--undirect_dataset', type=str,
                         choices=['Cora', 'CiteSeer', 'PubMed', 'Amazon-Photo', 'Amazon-Computers', 'Coauthor-CS'],
                         default='Amazon-Photo', help='data set selection as GraphSHA')
