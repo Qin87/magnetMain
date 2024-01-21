fd12cff5a14d1b16f48e95c0bb7b634b507c32bc
Qin87 <qinjiang19871018@gmail.com>
2024-01-21 21:38:39 +0000

    Revert "delete withAug" kkkkk This reverts commit 470d9b13852d8dedf251a3ada2ddb18072842806.

--- src/ArgsBen.py
+++ src/ArgsBen.py
@@ -6,22 +6,7 @@ def parse_args():
     # choices=["baseline--graph attention.", "baseline--Digraph"]
 
     # change frequently
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    parser.add_argument('--AugDirect', type=int, default=2, help='0 for without Aug, 1 for one direction, 2 for bidirection aug edges, '
-=======
-    parser.add_argument('--withAug', type=bool, default=True, help='with Aug or not')
     parser.add_argument('--AugDirect', type=int, default=0, help='1 for one direction, 2 for bidirection aug edges, '
->>>>>>> parent of 470d9b1 (delete withAug)
-=======
-    parser.add_argument('--withAug', type=bool, default=True, help='with Aug or not')
-    parser.add_argument('--AugDirect', type=int, default=0, help='1 for one direction, 2 for bidirection aug edges, '
->>>>>>> parent of 470d9b1 (delete withAug)
-=======
-    parser.add_argument('--withAug', type=bool, default=True, help='with Aug or not')
-    parser.add_argument('--AugDirect', type=int, default=0, help='1 for one direction, 2 for bidirection aug edges, '
->>>>>>> parent of 470d9b1 (delete withAug)
                                                                  '4 for bidegree and bidirection, 20 for my bidegree(best), 21 for graphSHA bidegree')
     parser.add_argument('--method_name', type=str, default='SAGE', help='method name')   # Tested OK: APPNP, SymDiGCN
     parser.add_argument('--GPUdevice', type=int, default=0, help='gpu 0,1,2 for selene')
